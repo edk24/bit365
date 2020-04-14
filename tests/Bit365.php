3282a2003b26b863a778f6fe3e054d2de40e0594
余晓波 <edk24@qq.com>
2020-04-14 13:12:21 +0800

    余晓波 update

--- tests/Bit365.php
+++ tests/Bit365.php
@@ -1,18 +1,12 @@
 <?
-
-/**
- * 数字格式化测试类
- * @author XinLiang
- */
-
 require '../vendor/autoload.php';
 
 use Bit365\Pay;
 
 // 创建订单并发送请求
-$order_no = md5(time());
-$a = new Pay('27029049', 'Zack');
-var_dump($a->create('27029049', 'alipay', $order_no, 0.01, md5(5))->send());
+$order_no = substr(md5(time() . time()), 0, 16);
+$a = new Pay('5', '');
+var_dump($a->create('27029049', 'alipay', $order_no, 299, md5('4'))->send());
 
 // 查询CNY和USDT的兑换比例
 var_dump($a->rateUSDTCNY());
